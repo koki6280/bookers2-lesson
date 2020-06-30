b99bd05f191bc05868c7a34d9548ca202ce7e84d
vagrant <vagrant@localhost.localdomain>
2020-06-30 02:29:56 +0000

    lesson

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '857816a6018190170db9d0c59f40c5ad64a0b6539de7ba445b734e3027a2461d29ca6d1c3e222549ecb5b363af9ae8cca88719b2b09cb7a13dd075b3c0f0b914'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
   resources :users
+  get 'top' => 'homes#top'
   root 'books#index'
 end
